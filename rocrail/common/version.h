e132e8f0eb152ea75d63d58a8e1d4aaf016a8133
GBert <info@gerhard-bertelsmann.de>
2021-12-16 12:26:18 +0100

    bump revision

--- rocrail/common/version.h
+++ rocrail/common/version.h
@@ -1,11 +1,12 @@
 const int revisionnr = 
-2006
+2007
 ;
 const char* commithash = 
-"b581ac6f4f0cad11a4d3c32959fb168afcfbbcfb";
+"02e3e27f278980d5e04e27b1e9c23ca1764b0860";
 const char* origin = "LOCAL"
 ;
 const char* revlog = 
+"2007 2021-12-16 11:27:49 +0100 readme.txt: update\n" \
 "2006 2021-12-15 13:10:13 +0100 weather: broadcast after active\n" \
 "2005 2021-12-15 12:55:58 +0100 wiopi,canusb: addressing correction\n" \
 "2004 2021-12-14 14:25:54 +0100 wiopi: canusb SOD\n" \
@@ -2005,6 +2006,5 @@ const char* revlog =
 "10 2019-09-22 09:07:53 +0200 obsolete icon deleted\n" \
 "9 2019-09-22 08:55:52 +0200 wiodlg: WIP\n" \
 "8 2019-09-22 08:43:23 +0200 clntcon: removed function to lookup duplets\n" \
-"7 2019-09-22 08:19:42 +0200 clntcon: disable removal of client duplets\n" \
 
 "log end";
