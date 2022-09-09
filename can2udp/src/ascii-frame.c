76e9760ed899cd8d3aefd398c2cd9a1b98be6d6d
GBert <info@gerhard-bertelsmann.de>
2022-09-09 04:16:53 +0200

    prepare TCP prt 15732 handling

---
+++ can2udp/src/ascii-frame.c
@@ -0,0 +1,22 @@
+/* ------------------------------------------- ---------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <info@gerhard-bertelsmann.de> wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return
+ * Gerhard Bertelsmann
+ * ----------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ascii-frame.h"
+
+int decode_ascii_frame(FILE *fd) {
+
+    fprintf(fd, "ok;");
+
+    return EXIT_SUCCESS;
+}
+
+
---
+++ can2udp/src/ascii-frame.h
@@ -0,0 +1,14 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <info@gerhard-bertelsmann.de> wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return
+ * Gerhard Bertelsmann
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _ASCII_FRAME_H_
+#define _ASCII_FRAME_H_
+
+#endif /* _ASCII_FRAME_H_ */
+
