f6a84586477d5e1783934b44a793abfa2bc0d03f
GBert <info@gerhard-bertelsmann.de>
2023-04-22 18:38:35 +0200

    create tmp dir and copy files - wip

--- z21/src/cs2toz21.c
+++ z21/src/cs2toz21.c
@@ -13,6 +13,8 @@
 
 
 /*
+ * Server sends initial packet
+ *
  * Epoch Time: 1664630770.036
  * Src: 192.168.0.171, Dst: 255.255.255.255
  * User Datagram Protocol, Src Port: 51312, Dst Port: 5728
@@ -29,11 +31,15 @@
  */
 
 /*
+ * Client answer
+ *
  * {"os":"android","appVersion":"1.4.6","deviceName":"Z21 Emulator","deviceType":"OpenWRT",
  * "request":"device_information_request","buildNumber":6076,"apiVersion":1}
  */
 
 /*
+ * Server sends file
+ *
  * {"owningDevice":{"os":"ios","appVersion":"1.4.6","deviceName":"iPad von Gerhard","deviceType":"iPad7,3",
  *  "request":"device_information_request","buildNumber":6076,"apiVersion":1},"fileName":"MeineAnlage.z21",
  *  "request":"file_transfer_info","fileSize":744444}
@@ -56,14 +62,19 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
+
 #include <sqlite3.h>
+#include <uuid/uuid.h>
 
 #include "read-cs2-config.h"
 #include "fmapping.h"
+#include "geturl.h"
 
 #define MAXDG		4096	/* maximum datagram size */
 #define MAXLINE         256
 #define Z21PORT		5728
+
+#define UUIDTEXTSIZE (sizeof(uuid_t) * 2) + 5
 
 char z21_fstring_none[] = "none";
 
@@ -251,6 +262,67 @@ int send_udp_broadcast(void) {
     return EXIT_SUCCESS;
 }
 
+char *create_directory(char *basedir, uuid_t *uuid) {
+    struct stat st;
+    char uuidtext[UUIDTEXTSIZE];
+    char *dir;
+
+    memset(&st, 0, sizeof st);
+    if (stat(basedir, &st)) {
+	fprintf(stderr, "%s: basedir \'%s\' doesn't exist\n", __func__, basedir);
+	return NULL;
+    }
+
+    memset(&st, 0, sizeof st);
+    asprintf(&dir, "%s/export", basedir);
+    if (stat(dir, &st) == -1) {
+	if (mkdir(dir, 0775)) {
+            fprintf(stderr, "%s: can't create %s\n", __func__, dir);
+	    return NULL;
+	}
+    }
+    free(dir);
+
+    uuid_unparse_upper(*uuid, uuidtext);
+    asprintf(&dir, "%s/export/%s", basedir, uuidtext);
+    if (mkdir(dir, 0775)) {
+	fprintf(stderr, "%s: can't create %s\n", __func__, dir);
+	return NULL;
+    }
+
+    return dir;
+}
+
+int copy_file(char *src, char *dst) {
+    char c[4096];
+    FILE *fd_in, *fd_out;
+
+    printf("copy file %s -> %s\n", src, dst);
+
+    fd_in = fopen(src, "r");
+    if (!fd_in) {
+	fprintf(stderr, "%s: can't fopen %s\n", __func__, src);
+        return EXIT_FAILURE;
+    }
+
+    fd_out = fopen(dst, "w");
+    if (!fd_out) {
+	fprintf(stderr, "%s: can't fopen %s\n", __func__, dst);
+        return EXIT_FAILURE;
+    }
+
+    while (!feof(fd_in)) {
+        size_t bytes = fread(c, 1, sizeof(c), fd_in);
+        if (bytes) {
+            fwrite(c, 1, bytes, fd_out);
+        }
+    }
+
+    fclose(fd_in);
+    fclose(fd_out);
+    return EXIT_SUCCESS;
+}
+
 int sql_update_history(sqlite3 * db) {
     char *err_msg;
     int ret;
@@ -264,17 +336,19 @@ int sql_update_history(sqlite3 * db) {
     return EXIT_SUCCESS;
 }
 
-int sql_insert_locos(sqlite3 * db) {
+int sql_insert_locos(sqlite3 *db, char *z21_dir, char *icon_dir, char *ip_s) {
     char *err_msg;
     int i, j, n, ret;
     struct loco_data_t *l;
     char *sql;
     char *z21_fstring;
+    uuid_t uuid;
+    char uuidtext[UUIDTEXTSIZE];
+    char *picture;
+    char *loco_icon_s, *loco_icon_d;
 
     i = 1;
     j = 1;
-    char *ip_s = "192.168.0.9";
-    char *uuid_s = "42849456-5902-4F87-951F-616E57387CA1.png";
 
     /* delete existing data */
     sql ="DELETE FROM vehicles;";
@@ -283,8 +357,21 @@ int sql_insert_locos(sqlite3 * db) {
     SQL_EXEC();
 
     for (l = loco_data; l != NULL; l = l->hh.next) {
+	uuid_generate(uuid);
+	uuid_unparse_upper(uuid, uuidtext);
+	asprintf(&picture, "%s.png", uuidtext);
+
+	// printf("Loco picture: %s.png\n", l->icon);
+        asprintf(&loco_icon_s, "%s/%s.png", icon_dir, l->icon);
+	asprintf(&loco_icon_d, "%s/%s", z21_dir, picture);
+	/* TODO: get picture false*/
+	if (copy_file(loco_icon_s, loco_icon_d) == EXIT_FAILURE) {
+            asprintf(&loco_icon_s, "%s/leeres Gleis.png", icon_dir);
+	    copy_file(loco_icon_s, loco_icon_d);
+	}
+
 	asprintf(&sql, "INSERT INTO vehicles VALUES(%d, '%s', '%s', 0, %d, %d, 1, %d, '', '', 0, '', '', '', '', '', '', '', '', '', '', '', "
-		       "0, '', 0, '%s', 0, 0, 0, 786, 0, 0, 1024, '', 0, 0);", i, l->name, uuid_s, l->tmax, l->uid, i - 1, ip_s);
+		       "0, '', 0, '%s', 0, 0, 0, 786, 0, 0, 1024, '', 0, 0);", i, l->name, picture , l->tmax, l->uid, i - 1, ip_s);
 	/* printf("%s\n", sql); */
 	SQL_EXEC();
 	for (n = 0; n < 32; n++) {
@@ -303,6 +390,7 @@ int sql_insert_locos(sqlite3 * db) {
 	}
 	i++;
     }
+    free(sql);
     return EXIT_SUCCESS;
 }
 
@@ -311,6 +399,9 @@ int main(int argc, char **argv) {
     char *loco_file;
     int opt, ret;
     sqlite3 *db;
+    char *z21_dir, *sql_file;
+    uuid_t z21_uuid;
+
 
     config_data.verbose = 1;
 
@@ -344,9 +435,22 @@ int main(int argc, char **argv) {
     if (config_data.verbose == 1)
 	printf("locos in CS2 File: %u\n", HASH_COUNT(loco_data));
 
+    /* preparing export directory */
+    uuid_generate(z21_uuid);
+    z21_dir = create_directory("/tmp", &z21_uuid);
+    if (!z21_dir) {
+	fprintf(stderr, "problems creating export directory\n");
+	return EXIT_FAILURE;
+    }
+
     /* open empty SQLite database */
 
-    ret = sqlite3_open("Loco_empty.sqlite", &db);
+    printf("storing %s\n", z21_dir);
+
+    asprintf(&sql_file, "%s/Loco.sqlite", z21_dir);
+
+    ret = copy_file("Loco_empty.sqlite", sql_file);
+    ret = sqlite3_open(sql_file, &db);
 
     if (ret != SQLITE_OK) {
 
@@ -356,7 +460,7 @@ int main(int argc, char **argv) {
 	return EXIT_FAILURE;
     }
     sql_update_history(db);
-    sql_insert_locos(db);
+    sql_insert_locos(db, z21_dir, "/var/www/html/icons", "192.168.0.9");
     send_udp_broadcast();
     sqlite3_close(db);
     return EXIT_SUCCESS;
