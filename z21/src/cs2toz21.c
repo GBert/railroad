fc8e3bffe3cbc70c62fdbbeb167f6c25e3e6b720
GBert <info@gerhard-bertelsmann.de>
2023-05-16 09:00:35 +0200

    add missing usage text

--- z21/src/cs2toz21.c
+++ z21/src/cs2toz21.c
@@ -97,7 +97,7 @@ unsigned char udpframe[MAXDG];
 extern struct loco_data_t *loco_data;
 
 void print_usage(char *prg) {
-    fprintf(stderr, "\nUsage: %s -c config_dir\n", prg);
+    fprintf(stderr, "\nUsage: %s -v -c config_dir -i <interface list> -s <config link> -p <icons link>\n", prg);
     fprintf(stderr, "   Version 0.94\n\n");
     fprintf(stderr, "         -c <config_dir>     set the config directory - default %s\n", config_data.config_dir);
     fprintf(stderr, "         -i <interface list> interface list - default %s\n", INTERFACE_LIST);
