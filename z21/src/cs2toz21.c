d263d3198486ac00e6482e64c4bd89625e246b88
GBert <info@gerhard-bertelsmann.de>
2023-06-15 12:30:45 +0200

    use new version number

--- z21/src/cs2toz21.c
+++ z21/src/cs2toz21.c
@@ -102,7 +102,7 @@ extern struct loco_data_t *loco_data;
 
 void print_usage(char *prg) {
     fprintf(stderr, "\nUsage: %s -v -c <config_dir> -i <interface list> -s <config link> -p <icons link>\n", prg);
-    fprintf(stderr, "   Version 0.98\n\n");
+    fprintf(stderr, "   Version 0.99\n\n");
     fprintf(stderr, "         -a <time_out>       try to find CS2/CS2 for <time_out> seconds using -i <interface list>\n");
     fprintf(stderr, "         -c <config_dir>     set the config directory - default %s\n", config_data.config_dir);
     fprintf(stderr, "         -i <interface list> interface list - default %s\n", INTERFACE_LIST);
