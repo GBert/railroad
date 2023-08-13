031fbbfd08b234d1614e0af145db2badf631a319
GBert <info@gerhard-bertelsmann.de>
2023-08-13 15:02:13 +0200

    use original timestamp

--- can2udp/files/scripts/get-icons.sh
+++ can2udp/files/scripts/get-icons.sh
@@ -17,14 +17,15 @@ wget $LINK -O $FILE
 mkdir -p $MNT_POINT
 mount -o loop -t $FS_TYPE $FILE $MNT_POINT
 
-cp -r $MNT_POINT/home/cs2/fcticons $WEB_DIR
-cp -r $MNT_POINT/home/cs2/icons $WEB_DIR
-cp -r $MNT_POINT/home/cs2/magicons_ $WEB_DIR
-mkdir -p $WEB_DIR/update
-cp $MNT_POINT/home/cs2/update/*.bin $WEB_DIR/update
-cp $MNT_POINT/home/cs2/update/*.ms2 $WEB_DIR/update
-# cp -r $MNT_POINT/home/cs2/spiel $WEB_DIR
-cp -r $MNT_POINT/home/cs2/doc $WEB_DIR
+cp -rp $MNT_POINT/home/cs2/fcticons $WEB_DIR
+cp -rp $MNT_POINT/home/cs2/icons $WEB_DIR
+cp -rp $MNT_POINT/home/cs2/magicons_ $WEB_DIR
+cp -rp $MNT_POINT/home/cs2/doc $WEB_DIR
+mkdir -rp $WEB_DIR/update
+cp -p $MNT_POINT/home/cs2/update/*.bin $WEB_DIR/update
+cp -p $MNT_POINT/home/cs2/update/*.ms2 $WEB_DIR/update
+# cp -rp $MNT_POINT/home/cs2/spiel $WEB_DIR
+cp -rp $MNT_POINT/home/cs2/doc $WEB_DIR
 
 umount $MNT_POINT
 
