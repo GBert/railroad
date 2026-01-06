4d27dcfbb8fbbff6f9e1d7b22843fec47c3924f9
GBert <info@gerhard-bertelsmann.de>
2026-01-06 15:18:43 +0100

    new version from Ralf

--- ms2-card-esp8266/Projects/Lok Karten Emulator/src/main.cpp
+++ ms2-card-esp8266/Projects/Lok Karten Emulator/src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
+#include <FS.h>
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <DNSServer.h>
 
-#include "Wire.h"
 #include "I2C_eeprom.h"
 #include "BR_xx.h"
+#include "loco_sdcard.h"
+#include "webserver.h"
 
 #define SPI_MISO    12
 #define SPI_MOSI    13
@@ -16,80 +23,95 @@
 #define I2C_SCL      5
 #define Card_Change 16
 
-#define LOCO_SIZE   272
+#define LOCO_BIN_SIZE      272
+#define LOCO_BIN_SIZE_MAX 8192
+
+const char *ssid = "LokkartenEMU";
+const char *password = "bingbing";
+IPAddress local_IP(192,168,4,1);
+IPAddress gateway(192,168,4,1);
+IPAddress subnet(255,255,255,0);
+ESP8266WebServer server(80);
+
+const char *ESPHostname = "LokEmu";
+const byte DNS_PORT = 53;
+DNSServer dnsServer;
 
 I2C_eeprom ee(0x50, I2C_DEVICESIZE_24LC64);
-uint8_t ee_buffer[LOCO_SIZE];
+uint8_t ee_buffer[LOCO_BIN_SIZE_MAX];
 uint8_t loco_id = 10;
-int sd_card;
+int sd_card, cycle_count, sd2ms2;
 File root;
+
 
 /* Test with simple BIN File of BR86 */
 void copy_loco_data(){
   uint16_t i;
-  for (i = 0; i < LOCO_SIZE; i++) {
+  for (i = 0; i < LOCO_BIN_SIZE; i++) {
     ee_buffer[i] = pgm_read_byte_far(&BR86_bin[i]);
   }
 }
 
-int write_loco2fram() {
-  int ret;
+int write_loco2fram(unsigned int size) {
+  int ret; /* TODO*/
 
-  /* access I2C memory by ESP8266 */
-  digitalWrite(I2C_Select, 1);
+  if (size > sizeof(ee_buffer)) {
+    Serial.println("BIN to large for FRAM");
+    return -1;
+  } else {
+    /* access I2C memory by ESP8266 */
+    digitalWrite(I2C_Select, 1);
 
-  ret = ee.isConnected();
-  if (!ret) {
-    Serial.printf("Error: 0x%20X Can't find FRAM", ret);
-    Serial.println();
-  }
+    ret = ee.isConnected();
+    if (!ret) {
+      Serial.printf("Error: 0x%20X Can't find FRAM", ret);
+      Serial.println();
+      digitalWrite(I2C_Select, 0);
+      return ret;
+    }
 
-  ret = ee.writeBlock(0, ee_buffer, LOCO_SIZE);
-  if (ret) {
-    Serial.printf("Error: 0x%20X Can't write FRAM", ret);
-    Serial.println();   
+    ret = ee.writeBlock(0, ee_buffer, size);
+    if (ret) {
+      Serial.printf("Error: 0x%20X Can't write FRAM", ret);
+      Serial.println();
+      digitalWrite(I2C_Select, 0);
+      return ret;   
+    }
   }
   /* release I2C memory - back to MS2 */
   digitalWrite(I2C_Select, 0);
-  return ret;
-}
-
-void printDirectory(File dir, int numTabs) {
-  while (true) {
-
-    File entry =  dir.openNextFile();
-    if (! entry) {
-      // no more files
-      break;
-    }
-    for (uint8_t i = 0; i < numTabs; i++) {
-      Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory()) {
-      Serial.println("/");
-      printDirectory(entry, numTabs + 1);
-    } else {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
-  }
+  return 0;
 }
 
 void setup() {
-  delay(3000);
-  Serial.begin(115200);
-  Serial.println(__FILE__);
-  Serial.println("Serial EEPROM version:");
-  Serial.println(I2C_EEPROM_VERSION);
-
-  
+  /* make emu passive first */
   pinMode(Card_Change, OUTPUT);
   pinMode(I2C_Select, OUTPUT);
   digitalWrite(Card_Change, 0);
-  copy_loco_data();
+  digitalWrite(I2C_Select, 0);
+  delay(2000);
+
+  Serial.begin(115200);
+
+  Serial.println("Serial EEPROM version:");
+  Serial.println(I2C_EEPROM_VERSION);
+
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+  Serial.print("Setting soft-AP ... ");
+  Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
+  Serial.print("Soft-AP IP address = ");
+  Serial.println(WiFi.softAPIP());
+  delay(100);
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "*", local_IP);
+
+  server.on("/", handle_OnConnect);
+  server.on("/sd2ms2_go", handle_OnConnect_sd2ms2_go);
+  server.on("/sd2ms2_stop", handle_OnConnect_sd2ms2_stop);
+  server.onNotFound(handle_NotFound);
+  server.begin();
+  
 
   Serial.print("Initialising SD Card ... ");
   if (!SD.begin(SPI_CS)) {
@@ -112,27 +134,14 @@ void setup() {
   Wire.begin(I2C_SDA, I2C_SCL);
   Wire.setClock(100000);
   ee.begin();
-  write_loco2fram();
+  //copy_loco_data();
+  //write_loco2fram(LOCO_BIN_SIZE);
 }
 
-
 void loop() {
-  Serial.println("Cycle");
-  delay(2000);
-  loco_id = loco_id +1;
-  if (loco_id > 70)
-     loco_id = 10;
-  ee_buffer[246]++;
-  if (ee_buffer[246] > 0x39) {
-    ee_buffer[246]=0x30;
-  }
-  ee_buffer[5]=loco_id;
-  ee_buffer[11]=loco_id;
-  Serial.printf("Loco ID: %d\n", loco_id);
-  digitalWrite(Card_Change, 0);
-  write_loco2fram();
-  Serial.println("Card Change");
+  server.handleClient();
+  dnsServer.processNextRequest(); //DNS
+  Serial.printf("Cycle %d\n", cycle_count++);
   delay(500);
-  digitalWrite(Card_Change, 1);
 }
 
